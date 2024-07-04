dcb1e6a0a28b8a25781160c234c90144f7cda99c
ShuntaToda <todashuntawork@gmail.com>
2024-07-04 14:36:13 +0900

    routerをRouteProviderに変更

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Main } from './pages/Main';
 import { SignUp } from './pages/SignUp';
@@ -8,21 +8,23 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [token, setToken] = useState("")
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />} >
+        <Route path='/' element={<Main />} />
+        <Route path='/signup' element={<SignUp setToken={setToken} />} />
+        <Route path='/signin' element={<SignIn setToken={setToken} />} />
+      </Route>
+    )
+  )
   useEffect(() => {
     console.log(token);
   }, [token])
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Layout />} >
-            <Route path='/' element={<Main />} />
-            <Route path='/signup' element={<SignUp setToken={setToken} />} />
-            <Route path='/signin' element={<SignIn setToken={setToken} />} />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
